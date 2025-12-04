f3319cfa6322a503deec4f0bc6eaf6232f08b22b
ecopony <ecopony@gmail.com>
2025-12-03 21:47:55 -0800

    Add MultiBlocProvider (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import { MapView } from './components/Map'
-import { ControlPanel } from './components/ControlPanel'
-import { MapBloc, MapBlocProvider } from './blocs/map'
-import { AirQualityBloc, AirQualityBlocProvider } from './blocs/air_quality'
-import { Sf311Bloc, Sf311BlocProvider } from './blocs/sf311'
+import { MapView } from "./components/Map";
+import { ControlPanel } from "./components/ControlPanel";
+import { MultiBlocProvider } from "./blocs/bloc_provider";
+import { MapBloc, MapBlocProvider } from "./blocs/map";
+import { AirQualityBloc, AirQualityBlocProvider } from "./blocs/air_quality";
+import { Sf311Bloc, Sf311BlocProvider } from "./blocs/sf311";
 
-const mapBloc = new MapBloc()
-const airQualityBloc = new AirQualityBloc()
-const sf311Bloc = new Sf311Bloc()
+const mapBloc = new MapBloc();
+const airQualityBloc = new AirQualityBloc();
+const sf311Bloc = new Sf311Bloc();
 
 // Wire up bloc-to-bloc communication
 // AirQualityBloc will reload data when map viewport changes significantly
-airQualityBloc.connectToMap(mapBloc)
+airQualityBloc.connectToMap(mapBloc);
 
 export default function App() {
   return (
-    <MapBlocProvider bloc={mapBloc}>
-      <AirQualityBlocProvider bloc={airQualityBloc}>
-        <Sf311BlocProvider bloc={sf311Bloc}>
-          <div className="w-full h-full relative">
-            <MapView />
-            <ControlPanel />
-          </div>
-        </Sf311BlocProvider>
-      </AirQualityBlocProvider>
-    </MapBlocProvider>
-  )
+    <MultiBlocProvider
+      blocs={[
+        { Provider: MapBlocProvider, bloc: mapBloc },
+        { Provider: AirQualityBlocProvider, bloc: airQualityBloc },
+        { Provider: Sf311BlocProvider, bloc: sf311Bloc },
+      ]}
+    >
+      <div className="w-full h-full relative">
+        <MapView />
+        <ControlPanel />
+      </div>
+    </MultiBlocProvider>
+  );
 }
